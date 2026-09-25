cabe9dec99d4ca29d35378978269ef01f44e92b6
liulibo <artlesbol@gmail.com>
2026-09-25 17:55:03 +0800

    Keep HCU CT leaf tuning out of decomposition children

--- src/plan/builder_context.cpp
+++ src/plan/builder_context.cpp
@@ -22,6 +22,7 @@ bool PlanBuilder::RequestContext::operator==(const RequestContext &other) const 
   return input_dtype == other.input_dtype && output_dtype == other.output_dtype &&
          device_type == other.device_type && device_index == other.device_index &&
          device_arch == other.device_arch && origin_rank == other.origin_rank &&
+         requested_n == other.requested_n &&
          batch == other.batch && ix_short_single == other.ix_short_single &&
          max_dynamic_smem_bytes == other.max_dynamic_smem_bytes;
 }
@@ -34,6 +35,7 @@ PlanBuilder::RequestContext PlanBuilder::make_request_context(const FFTRequest &
   context.device_index = request.device_index;
   context.device_arch = request.device_arch;
   context.origin_rank = request.origin_rank;
+  context.requested_n = request.requested_n;
   context.batch = request.batch;
   context.ix_short_single = request.requested_n == 1024 && ix_ct_single_policy_enabled(request);
   if (request.device_type == adaptor::backend_name()) {
--- src/utils/include/flagfft/plan.hpp
+++ src/utils/include/flagfft/plan.hpp
@@ -160,6 +160,7 @@ class PlanBuilder {
     int64_t device_index = -1;
     std::string device_arch;
     int64_t origin_rank = 0;
+    int64_t requested_n = 0;
     int64_t batch = 1;
     bool ix_short_single = false;
     int64_t max_dynamic_smem_bytes = kDynamicSmemFallbackBytes;
